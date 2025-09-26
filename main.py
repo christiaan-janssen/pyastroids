391859cf1b76913ab2fca8f0a9578fbcb1031ea0
Christiaan Janssen <christiaanjanssen@outlook.com>
2025-09-26 08:00:58 +0200

    add a player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,9 @@ def main():
                 return
 
         screen.fill(pygame.Color(0, 0, 0))
+
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # Delta time in seconds.
