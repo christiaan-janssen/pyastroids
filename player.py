a1d5647d4ed94fd5f9379aed22c38333bd7bd34d
Christiaan Janssen <christiaanjanssen@outlook.com>
2025-09-27 08:59:47 +0200

    limit the player shooting

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ from constants import (
     PLAYER_SHOOT_SPEED,
     PLAYER_TURN_SPEED,
     PLAYER_SPEED,
+    PLAYER_SHOOT_COOLDOWN,
     SHOT_RADIUS,
 )
 import pygame
@@ -14,6 +15,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -34,10 +36,15 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.shot_timer <= 0:
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = (
+                pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            )
 
     def update(self, dt):
+        self.shot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
